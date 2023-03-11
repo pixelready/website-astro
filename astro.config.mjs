45a3be00e8dc82483cb69aa54464d88eeb55564a
Owen Waring <bizartist@gmail.com>
2023-03-11 13:14:46 -0800

    Revert "experimenting with custom tailwind layers - pixel mask effect"
    This reverts commit 07e5fc6b031174e62f41dfbcc867fd4bb02a48f3.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    mdx(),
-    tailwind({
-      config: { applyBaseStyles: false },
-    }),
-  ],
+	integrations: [mdx(), tailwind()],
 });
